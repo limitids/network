3623671caeb7aa036a51d5873b38090dd40839c5
Thomas <thomasrobertsbradford@gmail.com>
2022-03-18 21:08:23 -0500

    partial profile functionality, posts and profile styling done

---
+++ networkproj/network/views.py
@@ -0,0 +1,88 @@
+from django.contrib.auth import authenticate, login, logout
+from django.db import IntegrityError
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
+from django.urls import reverse
+from django import forms
+from .models import User,Post,Comment,Like
+
+class PostForm(forms.Form):
+    content = forms.CharField(label='')
+
+def index(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            content = form.cleaned_data['content']
+        Post.objects.create(content = content,author=request.user)
+
+    posts = Post.objects.all()
+    for post in posts:
+        post.likes = len(Like.objects.filter(post=post))
+    return render(request, "network/index.html",{
+        'form': PostForm,
+        'posts': posts
+    })
+
+
+def login_view(request):
+    if request.method == "POST":
+
+        # Attempt to sign user in
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        # Check if authentication successful
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "network/login.html", {
+                "message": "Invalid username and/or password."
+            })
+    else:
+        return render(request, "network/login.html")
+
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
+
+
+def register(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+
+        # Ensure password matches confirmation
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]
+        if password != confirmation:
+            return render(request, "network/register.html", {
+                "message": "Passwords must match."
+            })
+
+        # Attempt to create new user
+        try:
+            user = User.objects.create_user(username, email, password)
+            user.save()
+        except IntegrityError:
+            return render(request, "network/register.html", {
+                "message": "Username already taken."
+            })
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, "network/register.html")
+
+
+def profile(request,userid):
+    
+    if request.method == 'GET':
+        user = User.objects.get(id=userid)
+        user.posts = Post.objects.filter(author=user)
+
+        return render(request,'network/profile.html', {
+            'user':user,
+        })
